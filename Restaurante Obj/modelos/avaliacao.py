cf98073f7d5bf9e27d8093dc485a3722b9d60acd
PoringProgrammer <146344912+ViniciusBPrado@users.noreply.github.com>
2024-12-05 14:22:30 -0300

    Add files via upload

---
+++ Restaurante Obj/modelos/avaliacao.py
@@ -0,0 +1,7 @@
+class Avaliacao:
+    def __init__(self, cliente, nota):
+        self._cliente = cliente
+        self._nota = nota
+
+# nessa parte definimos uma classe avaliacao , onde   vai receber 2 parametros , cliente e nota.
+# quando coloca o _ antes da da variavel, _cliente, isso informa que é pro usuário tomar cuidado ao mecher nesse local pois não é pra ser alterado
